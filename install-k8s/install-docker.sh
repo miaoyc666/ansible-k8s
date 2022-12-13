9e35a27816ec8eb2fc3355053370057b9394c421
miaoyc <miaoyc1989@hotmail.com>
2022-12-13 14:24:04 +0800

    ====feat: add -y====

--- install-k8s/install-docker.sh
+++ install-k8s/install-docker.sh
@@ -2,20 +2,20 @@
 apt remove docker docker.io containerd runc
 
 # 安装依赖
-apt install ca-certificates curl gnupg lsb-release
-apt install software-properties-common
+apt install -y ca-certificates curl gnupg lsb-release
+apt install -y software-properties-common
 
 # 添加密钥
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
 
 # 配置源
-add-apt-repository "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository -y "deb [arch=amd64] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 
 # 更新源
 apt -y update
 
 # 安装docker-ce
-apt install docker-ce
+apt install -y docker-ce
 
 # 启动
 systemctl enable docker
